dc33ff37614a00fa6a1103a18297813c9e865c58
Amin Arria <arria.amin@gmail.com>
2016-05-11 15:48:42 -0400

    Clases para recarga y consumo

--- Tarea3/Billetera.py
+++ Tarea3/Billetera.py
@@ -3,21 +3,23 @@ Created on 11 de may. de 2016
 
 @author: Alex & Amin
 '''
-"""
-class recargas():
-    def __init__(self, monto, fecha, Id):
-        self.monto = monto
-        self.fecha = fecha
-        self.Id = Id
 
-class consumos():
+from datetime import timedelta, datetime
+
+class Recarga():
     def __init__(self, monto, fecha, Id):
         self.monto = monto
         self.fecha = fecha
-        self.Id = Id
-"""     
+        self.ID = Id
+
+class Consumo():
+    def __init__(self, monto, fecha, Id):
+        self.monto = monto
+        self.fecha = fecha
+        self.ID = Id
+
 class BilleteraElectronica():
-    
+
     def __init__(self, Id=None, nombres=None, apellidos=None, ci=None, PIN=None):
         self.ID = Id
         self.Nombres = nombres
@@ -26,4 +28,3 @@ class BilleteraElectronica():
         self.PIN = PIN
         self.recargas = []
         self.debitos = []
-        
