0c74bea9d6f35ff9c1eeff4ae9962e223665ad12
Amin Arria <arria.amin@gmail.com>
2016-05-11 15:48:27 -0400

    TDD creacion Recarga y Consumo

--- Tarea3/test_Billetera.py
+++ Tarea3/test_Billetera.py
@@ -5,6 +5,7 @@ Created on 11 de may. de 2016
 '''
 import unittest
 from Billetera import *
+from datetime import timedelta, datetime
 
 
 class Test_BilleteraElectronica(unittest.TestCase):
@@ -18,8 +19,18 @@ class Test_BilleteraElectronica(unittest.TestCase):
         self.assertEqual(BilleteraPrueba.PIN , "abc123", "El PIN debe ser abc123")
         self.assertEqual(BilleteraPrueba.recargas , [], "Las recargas deben ser una lista vacia")
         self.assertEqual(BilleteraPrueba.debitos , [], "Los debitos deben ser una lista vacia")
-        
-    
+
+    def testRecarga(self):
+        recarga = Recarga(10, datetime(2016,5,11,15,0), 1)
+        self.assertEqual(recarga.monto, 10)
+        self.assertEqual(recarga.fecha, datetime(2016,5,11,15,0))
+        self.assertEqual(recarga.ID, 1)
+
+    def testConsumo(self):
+        consumo = Consumo(10, datetime(2016,5,11,15,0), 1)
+        self.assertEqual(consumo.monto, 10)
+        self.assertEqual(consumo.fecha, datetime(2016,5,11,15,0))
+        self.assertEqual(consumo.ID, 1)
 
 
 if __name__ == "__main__":
